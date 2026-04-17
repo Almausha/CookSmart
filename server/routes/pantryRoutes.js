8a40979dfa981ea1691bdf0131da016bd3e4075f
Reginavincenza <143274584+Reginavincenza@users.noreply.github.com>
2026-04-18 02:01:35 +0600

    changed logic

--- server/routes/pantryRoutes.js
+++ server/routes/pantryRoutes.js
@@ -1,14 +1,13 @@
 const express = require('express');
 const router  = express.Router();
 const { getPantryItems, createPantryItem, updatePantryItem, deletePantryItem } = require('../controllers/pantrycontroller');
-// const protect = require('../middleware/authMiddleware'); // will have to check if it's actually needed here or not.
 
 router.route('/')
-  .get(protect, getPantryItems)
-  .post(protect, createPantryItem);
+  .get(getPantryItems)
+  .post(createPantryItem);
 
 router.route('/:id')
-  .put(protect, updatePantryItem)
-  .delete(protect, deletePantryItem);
+  .put(updatePantryItem)
+  .delete(deletePantryItem);
 
 module.exports = router;
