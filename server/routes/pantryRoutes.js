febcdad863d601060c630b87adee4a6b395a9c5c
Reginavincenza <143274584+Reginavincenza@users.noreply.github.com>
2026-04-18 01:42:41 +0600

    nA

--- server/routes/pantryRoutes.js
+++ server/routes/pantryRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router  = express.Router();
 const { getPantryItems, createPantryItem, updatePantryItem, deletePantryItem } = require('../controllers/pantrycontroller');
-const protect = require('../middleware/authMiddleware'); // will have to check if it's actually needed here or not.
+// const protect = require('../middleware/authMiddleware'); // will have to check if it's actually needed here or not.
 
 router.route('/')
   .get(protect, getPantryItems)
