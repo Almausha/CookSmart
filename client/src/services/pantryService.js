e435fae2ca457cf0f40af1f3e7f931171c5f8a32
Reginavincenza <143274584+Reginavincenza@users.noreply.github.com>
2026-04-18 00:21:37 +0600

    api

---
+++ client/src/services/pantryService.js
@@ -0,0 +1,10 @@
+import axios from 'axios';
+const BASE = '/pantry';
+const api = axios.create({
+  baseURL: 'http://localhost:5000/api',
+});
+
+export const getPantryItems    = ()           => api.get(BASE);
+export const createPantryItem  = (pantrydata)       => api.post(BASE, pantrydata);
+export const updatePantryItem  = (userId, pantrydata)   => api.put(`${BASE}/${userId}`, pantrydata);
+export const deletePantryItem  = (userId)         => api.delete(`${BASE}/${userId}`);
